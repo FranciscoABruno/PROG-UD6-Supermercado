19c6269e40361d026211276df5e65dd2f318a04b
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-04 13:39:45 +0100

    Update Cliente.java

--- src/com/supermercado/hiperdino/principal/Cliente.java
+++ src/com/supermercado/hiperdino/principal/Cliente.java
@@ -1,5 +1,14 @@
 package com.supermercado.hiperdino.principal;
 
+import java.util.Stack;
+
 public class Cliente {
-    
+    private String nombre;
+    private Stack<Producto> cestaDeCompra;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        this.cestaDeCompra = new Stack<>();
+    }
+
 }
