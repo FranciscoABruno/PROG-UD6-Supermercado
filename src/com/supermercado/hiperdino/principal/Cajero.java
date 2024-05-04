73be41bb40c220b4393b2ac5d9bc8cee15bc352f
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-04 13:47:41 +0100

    Update Cajero.java

--- src/com/supermercado/hiperdino/principal/Cajero.java
+++ src/com/supermercado/hiperdino/principal/Cajero.java
@@ -1,5 +1,15 @@
 package com.supermercado.hiperdino.principal;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Cajero {
-    
+    private int numero;
+    private Queue<Cliente> colaClientes;
+
+    public Cajero(int numero) {
+        this.numero = numero;
+        this.colaClientes = new LinkedList<>();
+    }
+
 }
