52fa83014e12d4dab9dd8a7e36afdc1bd1dde316
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-04 13:34:19 +0100

    Update Producto.java
    correción clase producto

--- src/com/supermercado/hiperdino/principal/Producto.java
+++ src/com/supermercado/hiperdino/principal/Producto.java
@@ -5,7 +5,25 @@ public class Producto {
     private double precio;
 
     public Producto(String nombre, double precio) {
-        this.nombre = this.nombre;
-        this.precio = this.precio;
+        this.nombre = nombre;
+        this.precio = precio;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
 }
