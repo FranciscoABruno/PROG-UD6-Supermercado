a257b7816b70156c5786d2e76ad6795302631ad3
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-04 13:31:05 +0100

    Update Producto.java

--- src/com/supermercado/hiperdino/principal/Producto.java
+++ src/com/supermercado/hiperdino/principal/Producto.java
@@ -4,5 +4,8 @@ public class Producto {
     private String nombre;
     private double precio;
 
-    
+    public Producto(String nombre, double precio) {
+        this.nombre = this.nombre;
+        this.precio = this.precio;
+    }
 }
