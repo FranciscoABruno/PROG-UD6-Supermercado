16a593ef45d5ef53bb54188fbd0ede69fb9eb696
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-04 14:37:57 +0100

    Update Cajero.java

--- src/com/supermercado/hiperdino/principal/Cajero.java
+++ src/com/supermercado/hiperdino/principal/Cajero.java
@@ -16,4 +16,37 @@ public class Cajero {
         colaClientes.add(cliente);
     }
 
+    public Cliente atenderCliente() {
+        return colaClientes.poll();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Queue<Cliente> getColaClientes() {
+        return colaClientes;
+    }
+
+    public void setColaClientes(Queue<Cliente> colaClientes) {
+        this.colaClientes = colaClientes;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cajero\n");
+        sb.append("===================================\n");
+        sb.append("* Número de caja: ").append(numero).append("\n");
+        sb.append("* Total de clientes: ").append(colaClientes.size()).append("\n");
+        sb.append("* Clientes en la fila:\n");
+        for (Cliente cliente : colaClientes) {
+            sb.append(cliente.getNombre()).append("\n");
+        }
+        sb.append("===================================\n");
+        return sb.toString();
+    }
 }
