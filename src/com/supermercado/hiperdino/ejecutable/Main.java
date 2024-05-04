e41ca31f54b7a873a1a1e5cb8e3992e587014c79
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-05-04 14:43:47 +0100

    Update Main.java

--- src/com/supermercado/hiperdino/ejecutable/Main.java
+++ src/com/supermercado/hiperdino/ejecutable/Main.java
@@ -1,5 +1,19 @@
 package com.supermercado.hiperdino.ejecutable;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import com.supermercado.hiperdino.principal.Cajero;
+import com.supermercado.hiperdino.principal.Cliente;
+
 public class Main {
-    
+    private static Cajero cajero;
+    private static boolean cajaAbierta = false;
+
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+         cajero = new Cajero(1); // Creamos un cajero con número 1
+        Queue<Cliente> clientesPendientes = new LinkedList<>();
+    }
 }
